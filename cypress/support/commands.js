aba1bd652e5935d380ce0546c0316cacf68b6ed3
Sabrina Ramos Silveira <srsilveira03@gmail.com>
2024-10-21 13:16:16 -0300

    Exercicio 1

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,15 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+Cypress.Commands.add('login', (username, password) => {
+    cy.visit('https://www.globalsqa.com/angularJs-protractor/registration-login-example/#/login')
+    cy.get('#username').type(username)
+    cy.get('#password').type(password)
+    cy.get('.btn-primary').click()
+})
+Cypress.Commands.add('acessar', (username, password) => {
+    cy.visit('https://moodle.inatel.br/login/index.php')
+    cy.get('#username').type(username)
+    cy.get('#password').type(password)
+    cy.get('#loginbtn').click()
+})
